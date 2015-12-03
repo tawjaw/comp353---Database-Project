c01e8d8a68730e5f5c0cd324155048c179f61b84
tawjaw <tawjaw@gmail.com>
2015-12-03 03:49:53 -0500

    added select_db in config.php

--- website/config.php
+++ website/config.php
@@ -5,6 +5,7 @@ $user = '';
 $pass = '';
 $database = '';
 $link = mysql_connect($server_name, $user, $pass);
+mysql_select_db($database);
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
